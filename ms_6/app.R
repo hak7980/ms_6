37633a28bc84e1c2dee2e54105c8bc59578759a8
hak7980 <hamidkhan@college.harvard.edu>
2020-04-04 13:57:21 +0500

    update

--- ms_6/app.R
+++ ms_6/app.R
@@ -1,79 +1,26 @@
 library(shiny)
-
 library(tidyverse)
-
 library(ggplot2)
 
 ui <- fluidPage(
-    
-    titlePanel(strong("ms_6")),
-    
+    titlePanel(strong("ms_6"), "ms_6"),
     sidebarLayout(
-        
         sidebarPanel(
-            
-            selectInput("countryInput", "Country",
-                        
-                choices = c("Choose one" = "", 
-                            
-                            "Australia", "England",
-                            "India", "Pakistan"))),
-        
-        mainPanel(
-            
-            plotOutput("coolplot")
-        ))
-    
-    )
+            selectInput("countryInput", "Country",         
+                        choices = c("Choose one" = "", "Australia", "England","India", "Pakistan"))),
+        mainPanel(plotOutput("plot"))))
 
 server <- function(input, output) {
+    load("./Data/data.RData")
+    output$plot <-renderPlot({   
+        filtered <-  subsetpk %>% filter(team==input$countryInput) %>% na.omit()
+        filtered %>% ggplot() + geom_point(aes(x = factor(year), 
+                                               group = factor(bracket),                                
+                                               color = factor(bracket), y = prop_won), 
+                                           size=2) +            
+            labs(title = "Proportion of Matches won Sorted by Runs Scored in the \n First Five Overs", subtitle = "Sorted According to Run Bracket - Period: 2010-2017",   x = "Year", y = "Proportion Won", col = "Runs Scored Bracket") +  labs(caption = "Source: cricsheet.org") + theme_classic()+  theme(legend.position = "right", axis.text.x = element_text(angle = 45, hjust = 1))})
     
-    load("./Data/data.Rdata")
     
-    output$coolplot <-renderPlot({
-            
-    filtered <- 
-        
-        subsetpk %>%
-                
-                filter(team==input$countryInput) 
-                
-    filtered %>%
-        
-        ggplot() + 
-                
-                geom_point(aes(x = factor(year), 
-                               
-                               group = factor(bracket), 
-                               
-                               color = factor(bracket), y = prop_won), 
-                           
-                           size=2) +
-                
-                labs(title = 
-                         
-                         "Proportion of Matches won Sorted by Runs Scored in the \n First Five Overs", 
-                     
-                     subtitle = "Sorted According to Run Bracket - Period: 2010-2017",
-                     
-                     x = "Year", 
-                     
-                     y = "Proportion Won",
-                     
-                     col = "Runs Scored Bracket") +
-                
-                labs(caption = "Source: cricsheet.org") +
-                
-                theme_classic()+ 
-                
-                theme(legend.position = "right",
-                      
-                axis.text.x = element_text(angle = 45, hjust = 1))})
-        
-        
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
-
-
